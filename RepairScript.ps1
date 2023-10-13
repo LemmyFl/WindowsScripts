24a2e1c81704a5474b295e02d327b6c506ea4276
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2023-10-13 22:21:21 +0200

    Update RepairScript.ps1

--- RepairScript.ps1
+++ RepairScript.ps1
@@ -1,3 +1,5 @@
+#### <Start-Process powershell -ArgumentList "-ep bypass -c (iex (irm https://raw.githubusercontent.com/LemmyFl/WindowsScripts/main/RepairScript.ps1))" -Verb RunAs> ### 
+
 #requires -version 2
 <#
 .SYNOPSIS
@@ -16,9 +18,9 @@
   <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
 
 .NOTES
-  Version:        0.0.1
+  Version:        0.0.2
   Author:         <Lennart Heidtmann>
-  Creation Date:  <10.10.2023>
+  Creation Date:  <13.10.2023>
   Purpose/Change: Initial script development
   
 .EXAMPLE
@@ -33,17 +35,64 @@
 
 Function CheckDISM()
 {
-DISM /online /cleanup-image /scanhealth
+    # Run DISM to scan the image for errors
+    Write-Host "DISM scan in progress, this may take a while...";
+    $void = DISM /online /cleanup-image /scanhealth 
+
+    # Check if DISM reported an error (Exit Code equal 0)
+    if ($LASTEXITCODE -eq "0" ) 
+    {
+    Write-Host "No Error found during the DISM scan."
+    }
+    else
+    {
+    # Run DISM to restore the image Health
+    Write-Host "Error found and reparing";
+    $void= DISM /online /cleanup-image /restorehealth
+
+    # Check if the restore was successful
+    if ($LASTEXITCODE -eq "0") 
+    {
+        Write-Output "Health has been restored successfully."
+    }
+    else
+    {
+        Write-Output "Failed to restore health. Check the DISM logs for more information (C:\windows\logs\dism\dism.log)."
+    }
+    }
+
 }
+
 
 Function CheckSFC()
 {
-SFC /Scannow
+    # Run SFC /scannow to scan the system files for corruption and missing files
+    Write-Host "SFC scan started"
+    $void = SFC /scannow
+
+    # Check if the sfc scan was without failure (Exit Code equal 0)
+    if ($LASTEXITCODE -eq "0")
+    {
+    Write-Host "No Error found during the SFC scan."
+    }
+    else
+    {
+    Write-Host "Error found during the 1st SFC scan, 2nd auto repairing startet to check if it is repaired..."
+    $void = SFC /scannow
+    }
+           if ($LASTEXITCODE -eq "0")
+           {Write-Host "No Error found during the 2nd SFC scan."
+           }
+           else
+           {
+           Write-Host "Error found during the 2nd SFC scan, check the SFC logs for more information."
+           }
 }
 
 Function CheckFilesystem()
 {
 # Get a list of all drive letters on the system
+Write-Host "Filesystem and Metadata scan in progress, this may take some time..."
 $driveLetters = Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk" | ForEach-Object { $_.DeviceID }
 
 # Loop through each drive and run chkdsk with
@@ -60,7 +109,7 @@ if ($LASTEXITCODE -ne 0)
 
 else
 {
-    Write-Output "No Error found"
+    Write-Output "No Error found on drive $driveLetters"
     }
 }
 }
@@ -68,5 +117,5 @@ else
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 CheckFilesystem
-#CheckDISM
-#CheckSFC
+CheckDISM
+CheckSFC
