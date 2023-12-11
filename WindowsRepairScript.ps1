2316e1231bcbb535d2bd1f38166c1162fa0cf80b
LemmyFl <123549154+LemmyFl@users.noreply.github.com>
2023-12-11 11:32:33 +0100

    Update WindowsRepairScript.ps1

--- WindowsRepairScript.ps1
+++ WindowsRepairScript.ps1
@@ -7,61 +7,6 @@
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-Function CheckDISM()
-{
-    # Run DISM to scan the image for errors
-    Write-Host "DISM scan in progress, this may take a while...";
-    $void = DISM /online /cleanup-image /scanhealth 
-
-    # Check if DISM reported an error (Exit Code equal 0)
-    if ($LASTEXITCODE -eq "0" ) 
-    {
-    Write-Host "No Error found during the DISM scan."
-    }
-    else
-    {
-      # Run DISM to restore the image Health
-      Write-Host "Error found and reparing";
-      $void= DISM /online /cleanup-image /restorehealth
-
-      # Check if the restore was successful
-      if ($LASTEXITCODE -eq "0") 
-        {
-          Write-Output "Health has been restored successfully."
-        }
-        else
-        {
-        Write-Output "Failed to restore health. Check the DISM logs for more information (C:\windows\logs\dism\dism.log)."
-        }
-    }
-
-}
-
-
-Function CheckSFC()
-{
-    # Run SFC /scannow to scan the system files for corruption and missing files
-    Write-Host "SFC scan started"
-    $void = SFC /scannow
-
-    # Check if the sfc scan was without failure (Exit Code equal 0)
-    if ($LASTEXITCODE -eq "0")
-    {
-    Write-Host "No Error found during the SFC scan."
-    }
-    else
-    {
-    Write-Host "Error found during the 1st SFC scan, 2nd auto repairing startet to check if it is repaired..."
-    $void = SFC /scannow
-    }
-           if ($LASTEXITCODE -eq "0")
-           {Write-Host "No Error found during the 2nd SFC scan."
-           }
-           else
-           {
-           Write-Host "Error found during the 2nd SFC scan, check the SFC logs for more information."
-           }
-}
 
 Function CheckFilesystem()
 {
@@ -88,8 +33,68 @@ else
 }
 }
 
+Function CheckDISM()
+{
+    # Run DISM to scan the image for errors
+    Write-Host "DISM scan in progress, this may take a while...";
+    $void = DISM /online /cleanup-image /scanhealth 
+
+    # Check if DISM reported an error (Exit Code equal 0)
+    if ($LASTEXITCODE -eq "0" ) 
+        {
+        Write-Host "No Error found during the DISM scan."
+        }
+    else
+        {
+        # Run DISM to restore the image Health
+        Write-Host "Error found and reparing";
+        $void= DISM /online /cleanup-image /restorehealth
+
+        # Check if the restore was successful
+        if ($LASTEXITCODE -eq "0") 
+            {
+            Write-Output "Health has been restored successfully."
+            }
+        else
+            {
+            Write-Output "Failed to restore health. Check the DISM logs for more information (C:\windows\logs\dism\dism.log)."
+            }
+        }
+
+}
+
+Function CheckSFC()
+{
+    # Run SFC /scannow to scan the system files for corruption and missing files
+    Write-Host "SFC scan started"
+    $void = SFC /scannow
+
+    # Check if the sfc scan was without failure (Exit Code equal 0)
+    if ($LASTEXITCODE -eq "0")
+    {
+    Write-Host "No Error found during the SFC scan."
+    }
+    else
+    {
+    Write-Host "Error found during the 1st SFC scan, 2nd auto repairing startet to check if it is repaired..."
+    $void = SFC /scannow
+    }
+           if ($LASTEXITCODE -eq "0")
+           {Write-Host "No Error found during the 2nd SFC scan."
+           }
+           else
+           {
+           Write-Host "Error found during the 2nd SFC scan, check the SFC logs for more information."
+           }
+}
+
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Start-Transcript -Force -path "C:\PS\Logs\WindowsRepairScript_$(Get-Date -Format 'yyyy_MM_dd_-_HH_mm').txt"
 
 CheckFilesystem
 CheckDISM
 CheckSFC
+
+Stop-Transcript
+pause
